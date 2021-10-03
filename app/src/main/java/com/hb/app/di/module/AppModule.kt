c9bb56dbb813c40f9688fae0074dbfdd5d45fa46
HaiBui <thehai.bui@gmail.com>
2021-10-03 12:06:11 +0700

    Review code

--- app/src/main/java/com/hb/app/di/module/AppModule.kt
+++ app/src/main/java/com/hb/app/di/module/AppModule.kt
@@ -19,48 +19,47 @@ import org.koin.dsl.module
 import timber.log.Timber
 
 val appModule = module {
-    single { RxBus() }
-    single<ICache> { AppCache(androidContext()) }
-    single<ImageHelper> { GlideImageHelper() }
-    single { androidContext().getSharedPreferences(androidContext().packageName, MODE_PRIVATE) }
-    single(named("token")) { getToken(get()) }
-    single { createNetworkVerifier(get()) }
+  single { RxBus() }
+  single<ICache> { AppCache(androidContext()) }
+  single<ImageHelper> { GlideImageHelper() }
+  single { androidContext().getSharedPreferences(androidContext().packageName, MODE_PRIVATE) }
+  single(named("token")) { getToken(get()) }
+  single { createNetworkVerifier(get()) }
 }
 
 private fun getToken(pref: SharedPreferences): String = pref.getString(TOKEN_TAG, "").orEmpty()
 
 class MyDebugTree : Timber.DebugTree() {
-    override fun createStackElementTag(element: StackTraceElement): String {
-        return String.format(
-            "[L:%s] [M:%s] [C:%s]",
-            element.lineNumber,
-            element.methodName,
-            super.createStackElementTag(element)
-        )
-    }
+  override fun createStackElementTag(element: StackTraceElement): String {
+    return String.format(
+      "[L:%s] [M:%s] [C:%s]",
+      element.lineNumber,
+      element.methodName,
+      super.createStackElementTag(element)
+    )
+  }
 }
 
 private fun createNetworkVerifier(context: Context): NetworkVerifier {
-    return object : NetworkVerifier {
-        override fun isConnected(): Boolean {
-            val connectivityManager =
-                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                val nw = connectivityManager.activeNetwork ?: return false
-                val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-                return when {
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                    // for other device how are able to connect with Ethernet
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                    // for check internet over Bluetooth
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-                    else -> false
-                }
-            } else {
-                val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-                return nwInfo.isConnected
-            }
+  return object : NetworkVerifier {
+    override fun isConnected(): Boolean {
+      val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+          actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+          actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+          // for other device how are able to connect with Ethernet
+          actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+          // for check internet over Bluetooth
+          actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+          else -> false
         }
+      } else {
+        val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+        return nwInfo.isConnected
+      }
     }
+  }
 }
